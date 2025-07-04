444bbc4a4377bd20c302f129895ee8d687defa1c
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-07-04 16:20:18 +0330

    fix bug

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -100,8 +100,13 @@ export default function Page() {
     setInputValue(todo.title);
     setIsModalOpen(true);
     setEditingTodoId(todo.id);
-    setInputValue('') // Clear input after setting? (Might be redundant)
+    setInputValue('');
   }
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setEditingTodoId(null);
+    setInputValue('');
+  };
 
   /**
    * Handle form submission for adding or updating a todo
@@ -115,8 +120,7 @@ const submitted = (event: React.FormEvent<HTMLFormElement>) => {
     return;
   }
 
-  setIsModalOpen(false);
-
+ 
   if (editingTodoId !== null) {
     const updatedTodos = todos.map(todo =>
       todo.id === editingTodoId ? { ...todo, title: inputValue } : todo
@@ -135,6 +139,7 @@ const submitted = (event: React.FormEvent<HTMLFormElement>) => {
   }
 
   setInputValue('');
+  closeModal();
 };
 
 
@@ -194,12 +199,12 @@ const submitted = (event: React.FormEvent<HTMLFormElement>) => {
             <div className='primaryShadow options__wrapper flex-col-center gap-2 absolute rounded-lg top-6 right-0 bg-[#151515] w-[127px] px-1 py-3 flex-row-center border border-[#152C2B]'>
               <div onClick={()=> setTodos([])}  className='hover:bg-[#152C2B] rounded py-1 px-2 flex items-center justify-between w-full hover:cursor-pointer'>
                 Delete all
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5"><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"></path></svg>
               </div>
               <div onClick={checkAll} className='hover:bg-[#152C2B] rounded py-1 px-2 flex items-center justify-between w-full hover:cursor-pointer'>
                 Check all
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                 </svg>
               </div>
             </div>
@@ -244,8 +249,8 @@ const submitted = (event: React.FormEvent<HTMLFormElement>) => {
                       <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
 
-                    <svg onClick={()=> updateTodo(todo)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 cursor-pointer">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    <svg onClick={()=> updateTodo(todo)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 cursor-pointer">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                     </svg>
                   </div>
                 </div>
@@ -265,34 +270,41 @@ const submitted = (event: React.FormEvent<HTMLFormElement>) => {
       </div>
       
       {isModalOpen && (
-      <>
-        {/* Modal Overlay */}
-        <div className='z-50 bg-black/80 flex-row-center min-h-screen absolute right-0 left-0 top-0 bottom-0'>
+        <>
+          {/* Modal Overlay */}
+          <div className='z-50 bg-black/80 flex-row-center min-h-screen absolute right-0 left-0 top-0 bottom-0'>
 
-          {/* Modal Box */}
-          <div className='primaryShadow bg-[#201F1F] w-[90%] sm:w-[50%] lg:w-[30%] rounded-md text-white'>
+            {/* Modal Box */}
+            <div className='primaryShadow bg-[#201F1F] w-[90%] sm:w-[50%] lg:w-[30%] rounded-md text-white'>
 
-            {/* Modal Header / Close Button */}
-            <div className='flex items-center justify-end bg-[#152C2B] px-3 py-4 rounded-t-md'>
-              <span onClick={()=> setIsModalOpen((prevState) => !prevState)} className='hover:cursor-pointer'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-8 h-8">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-              </span>
-            </div>
+              {/* Modal Header */}
+              <div className='flex items-center justify-between bg-[#152C2B] px-3 py-4 rounded-t-md'>
+                {/* Title */}
+                <h2 className="text-lg font-semibold">
+                  {editingTodoId !== null ? "Edit Todo" : "Add Todo"}
+                </h2>
+                
+                {/* Close Button */}
+                <span onClick={closeModal} className='hover:cursor-pointer' >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-8 h-8">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>
+                </span>
+              </div>
 
-            {/* Modal Content / Form */}
-            <div className='px-3 py-4'>
-              <form onSubmit={submitted} action="" className='flex-col-center gap-2'>
-                <input value={inputValue} onChange={inputHandler} type="text" placeholder='Add Todo...' className='border-b w-[70%]'/>
-                <button type="submit" className='border border-gray-600 px-11 py-2 mt-4 rounded-lg secoundaryShadow hover:bg-[#152C2B]'>Enter</button>
-              </form>
+              {/* Modal Content / Form */}
+              <div className='px-3 py-4'>
+                <form onSubmit={submitted} className='flex-col-center gap-2'>
+                  <input value={inputValue} onChange={inputHandler} type="text" placeholder={editingTodoId !== null ? "Edit your todo..." : "Add a new todo..."} className='border-b w-[70%] px-2 py-1 rounded' />
+                  <button type="submit" className='border border-gray-600 px-11 py-2 mt-4 rounded-lg secoundaryShadow hover:bg-[#152C2B]' >
+                    {editingTodoId !== null ? "Update Todo" : "Add Todo"}
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </>
+        </>
       )}
-
     </section>
   )
 }
